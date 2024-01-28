36c9b748c0ddb5b84c1aa61796a334a09bf58139
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2024-01-28 16:14:19 +0530

    Added server and database

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const PORT = 8080;
+const app = express();
+const cors = require("cors");
+// const PORT = process.env.PORT;
+dotenv.config();
+
+const mongoDB = require("./config/db");
+
+mongoDB();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE" // Include DELETE here
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(express.json());
+app.use("/images", express.static("images"));
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
