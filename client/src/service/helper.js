91d35998899e703506b53e2d3056cb8bd7efdcbc
Vivek Kumar <82724500+Vivek7282@users.noreply.github.com>
2024-02-07 05:09:25 +0530

    Update helper.js

--- client/src/service/helper.js
+++ client/src/service/helper.js
@@ -1,3 +1,4 @@
 // export const BASE_URL = "http://localhost:8080/api/v1/user";
 // export const SERVER_URL = "http://localhost:8080";
-export const BASE_URL = "https://paperhub.onrender.com/api/v1/user";
+// export const BASE_URL = "https://paperhub.onrender.com/api/v1/user";
+export const BASE_URL = "https://easy-ruby-raven-belt.cyclic.app/api/v1/user";
