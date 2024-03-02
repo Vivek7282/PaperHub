9a35e17ba192233735c46f13c25e340d8a561200
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2024-03-02 19:46:45 +0530

    Deployment

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(
+      "mongodb+srv://kumarvivek7282827749:vivekkumar@cluster0.qjxcqgc.mongodb.net/PaperHub?retryWrites=true&w=majority"
+    );
     console.log(`Mongodb connected ${mongoose.connection.host}`);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 require("dotenv").config();
-const PORT = 8080;
+const PORT = 9090;
 const app = express();
 const cors = require("cors");
+const path = require("path");
 // const PORT = process.env.PORT;
 dotenv.config();
 
@@ -11,27 +12,27 @@ const mongoDB = require("./config/db");
 const authMiddleware = require("./middlewares/authMiddleware");
 const { authController } = require("./controller/userController");
 mongoDB();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE" // Include DELETE here
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 // app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE" // Include DELETE here
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
 //   next();
 // });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use("/images", express.static("images"));
 app.use("/api/v1/user", require("./routers/AddQuestion"));
@@ -39,9 +40,9 @@ app.use("/api/v1/user", require("./routers/Operations"));
 app.use("/api/v1/user", require("./routers/Login"));
 app.use("/api/v1/user", require("./routers/Register"));
 app.post("/api/v1/user/getUserData", authMiddleware, authController);
-app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 app.listen(PORT, () => {
   console.log(`server is running at ${PORT}`);
